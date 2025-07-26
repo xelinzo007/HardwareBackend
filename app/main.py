d0abb7cdf31d0d1066b6363998140b692cdb5016
sujith.s <sujith.s@mfilterit.com>
2025-07-26 17:52:47 +0530

    updated the data

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Construction Hardware Shop")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://globalbuildhub.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
