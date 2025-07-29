62d87d3ec7f3457d2a3e141680fe7c98f404c9d1
sujith.s <sujith.s@mfilterit.com>
2025-07-29 09:13:19 +0530

    updated the data

--- app/db/models/customer.py
+++ app/db/models/customer.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from app.db.database import Base
+from app.db.base import Base
 
 class Customer(Base):
     __tablename__ = "customers"
